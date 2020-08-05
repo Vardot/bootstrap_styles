b5f2c0086328f72e02977ace40772c67c15f635b
Aaron Christian <aaronc@imagexmedia.com>
2020-08-04 19:57:47 -0700

    Issue #3163320 by AaronChristian: Edit Add Effects to styles tab with backend configurations

--- src/Plugin/BootstrapStyles/Style/ScrollEffects.php
+++ src/Plugin/BootstrapStyles/Style/ScrollEffects.php
@@ -14,7 +14,7 @@ use Drupal\Core\Form\FormStateInterface;
  *   id = "scroll_effects",
  *   title = @Translation("Scroll Effects"),
  *   group_id = "animation",
- *   weight = 1
+ *   weight = 3
  * )
  */
 class ScrollEffects extends StylePluginBase {
@@ -32,13 +32,46 @@ class ScrollEffects extends StylePluginBase {
       '#open' => TRUE,
     ];
 
+    // Library assets
+    $form['animation']['scroll_effects_library_type'] = [
+      '#type' => 'select',
+      '#default_value' => $config->get('scroll_effects_library_type'),
+      '#title' => $this->t('How should we load the animation library?'),
+      '#required' => TRUE,
+      '#options' => [
+        'local' => $this->t('Do nothing, my theme handle it'),
+        'external' => $this->t('Add the library for me please'),
+      ],
+      '#description' => $this->t('<p>Default uses the AOS library: <a href="https://michalsnik.github.io/aos" target="_blank">https://michalsnik.github.io/aos</a><br/> You can override the animation library in your theme by using <code>libraries-override: bootstrap_styles/plugin.scroll_effects.build: your_theme/your_new_library_definition</code> <small>https://www.drupal.org/node/2497313</small></p> '),
+    ];
+
     $form['animation']['scroll_effects'] = [
       '#type' => 'textarea',
       '#default_value' => $config->get('scroll_effects'),
-      '#title' => $this->t('Scroll Effects (classes)'),
-      '#description' => $this->t('<p>Enter one value per line, in the format <b>key|label</b> where <em>key</em> is the CSS class name (without the .), and <em>label</em> is the human readable name of the effect.</p>'),
+      '#title' => $this->t('Scroll Effects'),
+      '#description' => $this->t('<p>Enter one value per line, in the format <b>key|label</b> where <em>key</em> is the attribute\'s value, and <em>label</em> is the human readable name of the effect.</p>'),
       '#cols' => 60,
       '#rows' => 5,
+    ];
+
+    $form['animation']['scroll_effects_attr_type'] = [
+      '#type' => 'checkbox',
+      '#default_value' => $config->get('scroll_effects_attr_type'),
+      '#title' => $this->t('Use data attribute instead of class.'),
+    ];
+
+    $form['animation']['scroll_effects_data_key'] = [
+      '#type' => 'textfield',
+      '#default_value' => $config->get('scroll_effects_data_key'),
+      '#title' => $this->t('Data Key'),
+      '#description' => $this->t('<p>The <strong>data_key</strong> will be used as the data attribute. Example: <code>data_key="key"</code></p>'),
+      '#states' => [
+        'visible' => [
+          [
+            [':input[name="scroll_effects_attr_type"]' => ['checked' => TRUE]],
+          ],
+        ],
+      ]
     ];
 
     return $form;
@@ -49,6 +82,9 @@ class ScrollEffects extends StylePluginBase {
    */
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
     $this->config()
+      ->set('scroll_effects_library_type', $form_state->getValue('scroll_effects_library_type'))
+      ->set('scroll_effects_attr_type', $form_state->getValue('scroll_effects_attr_type'))
+      ->set('scroll_effects_data_key', $form_state->getValue('scroll_effects_data_key'))
       ->set('scroll_effects', $form_state->getValue('scroll_effects'))
       ->save();
   }
@@ -89,15 +125,37 @@ class ScrollEffects extends StylePluginBase {
    * {@inheritdoc}
    */
   public function build(array $build, array $storage, $theme_wrapper = NULL) {
+
+    $library_type = $this->config()->get('scroll_effects_library_type');
+    $attribute_type = $this->config()->get('scroll_effects_attr_type');
+    $data_key = $this->config()->get('scroll_effects_data_key');
+
     // Assign the style to element or its theme wrapper if exist.
     if ($theme_wrapper && isset($build['#theme_wrappers'][$theme_wrapper])) {
-      $build['#theme_wrappers'][$theme_wrapper]['#attributes']['class'][] = $storage['class'];
+      if (isset($attribute_type) && $attribute_type === 1) {
+        // Output some sort of data attribute.
+        $build['#theme_wrappers'][$theme_wrapper]['#attributes'][$data_key][] = $storage['class'];
+      }
+      else {
+        // Output classes.
+        $build['#theme_wrappers'][$theme_wrapper]['#attributes']['class'][] = $storage['class'];
+      }
     }
     else {
-      $build['#attributes']['class'][] = $storage['class'];
+      if (isset($attribute_type) && $attribute_type === 1) {
+        // Output some sort of data attribute.
+        $build['#attributes'][$data_key][] = $storage['class'];
+      }
+      else {
+        // Output classes.
+        $build['#attributes']['class'][] = $storage['class'];
+      }
     }
-    // $build['#attached']['library'][] = 'bootstrap_styles/plugin.scroll_effects.build';
+
+    if (isset($library_type) && $library_type === 'external') {
+      $build['#attached']['library'][] = 'bootstrap_styles/plugin.scroll_effects.build';
+    }
+
     return $build;
   }
-
 }
