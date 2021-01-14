0199e9629a32d8bdb640f6eb3dc7be1f944a27f7
RajabNatshah <RajabNatshah@1414312.no-reply.drupal.org>
2021-01-13 22:25:56 -0500

    Issue #3191446 by RajabNatshah: Fix base path issues with multilingual websites and if Drupal was located in a sub-directory

--- src/Plugin/BootstrapStyles/Style/BackgroundColor.php
+++ src/Plugin/BootstrapStyles/Style/BackgroundColor.php
@@ -50,7 +50,7 @@ class BackgroundColor extends StylePluginBase {
    * {@inheritdoc}
    */
   public function buildStyleFormElements(array &$form, FormStateInterface $form_state, $storage) {
-    $icon_path = base_path() . drupal_get_path('module', 'bootstrap_styles') . '/images/';
+    $icon_path = drupal_get_path('module', 'bootstrap_styles') . '/images/';
     $form['background_type']['#options']['color'] = $this->getSvgIconMarkup($icon_path . 'plugins/background/background-color.svg');
     $form['background_type']['#default_value'] = $storage['background']['background_type'] ?? 'color';
 
