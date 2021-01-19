496d41f58800f7853dccbc8f4ab5d88ab44e4410
Mahmoud Zayed <mahmoudeid2050@gmail.com>
2021-01-18 23:31:59 -0500

    Add fields for Style form breakpoints.

--- src/ResponsiveTrait.php
+++ src/ResponsiveTrait.php
@@ -66,7 +66,7 @@ trait ResponsiveTrait {
    * @param \Drupal\Core\Form\FormStateInterface $form_state
    *   The current state of the form.
    * @param array $fields
-   *   The array of fields, each field has its parents and keyed by field name.
+   *   The array of field names.
    */
   protected function submitBreakpointsConfigurationForm(FormStateInterface $form_state, array $fields) {
     // Loop through the fields.
@@ -76,6 +76,74 @@ trait ResponsiveTrait {
         $this->config()
           ->set($field_name . '_' . $breakpoint, $form_state->getValue($field_name . '_' . $breakpoint))
           ->save();
+      }
+    }
+  }
+
+  /**
+   * Build the breakpoints style form elements.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param array $fields
+   *   The array of fields, the style options key and keyed by field name.
+   */
+  protected function buildBreakpointsStyleFormElements(array &$form, array $fields) {
+    // Loop through the fields.
+    foreach ($fields as $field_name => $style_options_key) {
+      // Loop through the breakpoints.
+      foreach ($this->getBreakpointsKeys() as $breakpoint) {
+        $form[$field_name . '_' . $breakpoint] = $form[$field_name];
+        $form[$field_name . '_' . $breakpoint]['#options'] = $this->getStyleOptions($style_options_key . '_' . $breakpoint);
+
+        $form[$field_name . '_' . $breakpoint]['#default_value'] = $storage[$field_name . '_' . $breakpoint]['class'] ?? NULL;
+        $form[$field_name . '_' . $breakpoint]['#states']['visible'][':input.bs_responsive']['value'] = $breakpoint;
+        // Hide the generic one.
+        $form[$field_name]['#states']['visible'][':input.bs_responsive']['value'] = 'all';
+      }
+    }
+  }
+
+  /**
+   * Save the breakpoints fields values for style form.
+   *
+   * @param array $group_elements
+   *   The submitted form values array.
+   * @param array $storage
+   *   An associative array containing the form storage.
+   * @param array $fields
+   *   The array of field names.
+   */
+  protected function submitBreakpointsStyleFormElements(array $group_elements, array &$storage, array $fields) {
+    // Loop through the fields.
+    foreach ($fields as $field_name) {
+      // Loop through the breakpoints.
+      foreach ($this->getBreakpointsKeys() as $breakpoint) {
+        $storage[$field_name . '_' . $breakpoint] = [
+          'class' => $group_elements[$field_name . '_' . $breakpoint],
+        ];
+      }
+    }
+  }
+
+  /**
+   * Add the breakpoints classes to the build classes.
+   *
+   * @param array $classes
+   *   An associative array containing build classes.
+   * @param array $storage
+   *   An associative array containing the form storage.
+   * @param array $fields
+   *   The array of field names.
+   */
+  protected function buildBreakpoints(array &$classes, $storage, array $fields) {
+    // Loop through the fields.
+    foreach ($fields as $field_name) {
+      // Loop through the breakpoints.
+      foreach ($this->getBreakpointsKeys() as $breakpoint) {
+        if (isset($storage[$field_name . '_' . $breakpoint]['class'])) {
+          $classes[] = $storage[$field_name . '_' . $breakpoint]['class'];
+        }
       }
     }
   }
