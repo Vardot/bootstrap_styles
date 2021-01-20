ae41de93d3bf5013d395f3f207da434b7793e75f
Mahmoud Zayed <mahmoudeid2050@gmail.com>
2021-01-20 17:35:05 -0500

    Create the breakpoints functions for index based fields.

--- src/ResponsiveTrait.php
+++ src/ResponsiveTrait.php
@@ -44,7 +44,7 @@ trait ResponsiveTrait {
       '#default_value' => 'all',
       '#validated' => TRUE,
       '#attributes' => [
-        'class' => ['bs_col--full', 'bs_responsive'],
+        'class' => ['bs_col--full', 'bs_responsive', 'bs_responsive_' . $group_name],
       ],
     ];
     // Loop through the breakpoints.
@@ -64,6 +64,43 @@ trait ResponsiveTrait {
   }
 
   /**
+   * Get breakpoint title by key.
+   *
+   * @return string
+   *   breakpoint title.
+   */
+  protected function getBreakpointTitle($breakpoint_key) {
+    return $this->getBreakpoints()[$breakpoint_key];
+  }
+
+  /**
+   * Build the breakpoints configuration form.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param array $fields
+   *   The array of fields, each field has its parents and keyed by field name.
+   */
+  protected function createBreakpointFormField(array &$form, string $breakpoint_key, string $field_name, array $original_parents, array $new_parents = []) {
+    // Loop through the breakpoints.
+    // foreach ($this->getBreakpoints() as $breakpoint_key => $breakpoint_value) {
+      // Get the original field.
+      $field = NestedArray::getValue($form, array_merge($original_parents, [$field_name]));
+
+      // Change field attributes.
+      $field['#title'] .= ' - ' . $this->getBreakpointTitle($breakpoint_key);
+      $field['#default_value'] = $this->config()->get($field_name . '_' . $breakpoint_key);
+      $target_parents = $original_parents;
+      if ($new_parents) {
+        $target_parents = $new_parents;
+      }
+      NestedArray::setValue($form, array_merge($target_parents, [$field_name . '_' . $breakpoint_key]), $field);
+    // }
+  }
+
+  /**
+   * TODO: Remove and replace this function.
+   *
    * Build the breakpoints configuration form.
    *
    * @param array $form
@@ -109,7 +146,108 @@ trait ResponsiveTrait {
   }
 
   /**
-   * Build the breakpoints style form elements.
+   * Create breakpoint field for a given field.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param string $field_name
+   *   The field name.
+   * @param string $breakpoint_key
+   *   The breakpoint key.
+   * @param string $group_id
+   *   The plugin group id.
+   * @param array $storage
+   *   An associative array containing the form storage.
+   * @param string $storage_key
+   *   The storage key of the field in the storage array if
+   *   it's different from the field name.
+   */
+  protected function createBreakpointStyleFormField(array &$form, string $field_name, string $breakpoint_key, string $group_id, array $storage, string $storage_key = NULL) {
+    $form[$field_name . '_' . $breakpoint_key] = $form[$field_name];
+    $form[$field_name . '_' . $breakpoint_key]['#options'] = $this->getStyleOptions($storage_key . '_' . $breakpoint_key);
+
+    $form[$field_name . '_' . $breakpoint_key]['#default_value'] = $storage[$field_name . '_' . $breakpoint_key]['class'] ?? NULL;
+    $form[$field_name . '_' . $breakpoint_key]['#states']['visible'][':input.bs_responsive_' . $group_id]['value'] = $breakpoint_key;
+    // Hide the generic one.
+    $form[$field_name]['#states']['visible'][':input.bs_responsive_' . $group_id]['value'] = 'all';
+  }
+
+  /**
+   * Create breakpoints fields for a given field.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param string $field_name
+   *   The field name.
+   * @param string $group_id
+   *   The plugin group id.
+   * @param array $storage
+   *   An associative array containing the form storage.
+   * @param string $storage_key
+   *   The storage key of the field in the storage array if
+   *   it's different from the field name.
+   */
+  protected function createBreakpointsStyleFormFields(array &$form, string $field_name, string $group_id, array $storage, string $storage_key = NULL) {
+    foreach ($this->getBreakpointsKeys() as $breakpoint_key) {
+      $this->createBreakpointStyleFormField($form, $field_name, $breakpoint_key, $group_id, $storage, $storage_key);
+    }
+  }
+
+  /**
+   * Create breakpoint field for class index based field like padding.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param string $field_name
+   *   The field name.
+   * @param string $breakpoint_key
+   *   The breakpoint key.
+   * @param string $group_id
+   *   The plugin group id.
+   * @param array $storage
+   *   An associative array containing the form storage.
+   * @param string $storage_key
+   *   The storage key of the field in the storage array if
+   *   it's different from the field name.
+   */
+  protected function createBreakpointStyleFormClassIndexBasedField(array &$form, string $field_name, string $breakpoint_key, string $group_id, array $storage, string $storage_key = NULL) {
+    $breakpoint_default_value = 0;
+    if (isset($storage[$field_name . '_' . $breakpoint_key]['class'])) {
+      $breakpoint_default_value = $this->getStyleOptionIndexByClass($field_name . '_' . $breakpoint_key, $storage[$field_name . '_' . $breakpoint_key]['class']);
+    }
+
+    $form[$field_name . '_' . $breakpoint_key] = $form[$field_name];
+    $form[$field_name . '_' . $breakpoint_key]['#options'] = $this->getStyleOptions($field_name . '_' . $breakpoint_key);
+
+    $form[$field_name . '_' . $breakpoint_key]['#default_value'] = $breakpoint_default_value;
+    $form[$field_name . '_' . $breakpoint_key]['#states']['visible'][':input.bs_responsive_' . $group_id]['value'] = $breakpoint_key;
+    // Hide the generic one.
+    $form[$field_name]['#states']['visible'][':input.bs_responsive_' . $group_id]['value'] = 'all';
+  }
+
+  /**
+   * Create breakpoints fields for class index based field like padding.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param string $field_name
+   *   The field name.
+   * @param string $group_id
+   *   The plugin group id.
+   * @param array $storage
+   *   An associative array containing the form storage.
+   * @param string $storage_key
+   *   The storage key of the field in the storage array if
+   *   it's different from the field name.
+   */
+  protected function createBreakpointsStyleFormClassIndexBasedFields(array &$form, string $field_name, string $group_id, array $storage, string $storage_key = NULL) {
+    foreach ($this->getBreakpointsKeys() as $breakpoint_key) {
+      $this->create_keyStyleFormClassIndexBasedField($form, $field_name, $breakpoint_key, $group_id, $storage, $storage_key);
+    }
+  }
+
+  /**
+   * TODO: RENAME Build the breakpoints style form elements.
    *
    * @param array $form
    *   An associative array containing the structure of the form.
@@ -117,8 +255,10 @@ trait ResponsiveTrait {
    *   The array of fields, the style options key and keyed by field name.
    * @param array $storage
    *   An associative array containing the form storage.
+   * @param string $group_id
+   *   The plugin group id.
    */
-  protected function buildBreakpointsStyleFormElements(array &$form, array $fields, array $storage) {
+  protected function buildBreakpointsStyleFormElements(array &$form, array $fields, array $storage, string $group_id) {
     // Loop through the fields.
     foreach ($fields as $field_name => $style_options_key) {
       // Loop through the breakpoints.
@@ -127,9 +267,9 @@ trait ResponsiveTrait {
         $form[$field_name . '_' . $breakpoint]['#options'] = $this->getStyleOptions($style_options_key . '_' . $breakpoint);
 
         $form[$field_name . '_' . $breakpoint]['#default_value'] = $storage[$field_name . '_' . $breakpoint]['class'] ?? NULL;
-        $form[$field_name . '_' . $breakpoint]['#states']['visible'][':input.bs_responsive']['value'] = $breakpoint;
+        $form[$field_name . '_' . $breakpoint]['#states']['visible'][':input.bs_responsive_' . $group_id]['value'] = $breakpoint;
         // Hide the generic one.
-        $form[$field_name]['#states']['visible'][':input.bs_responsive']['value'] = 'all';
+        $form[$field_name]['#states']['visible'][':input.bs_responsive_' . $group_id]['value'] = 'all';
       }
     }
   }
