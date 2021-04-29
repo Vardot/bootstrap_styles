3dd888a97c898fd05c298b7edfb29884b55046da
mahmoud-zayed <mahmoudeid@2947001.no-reply.drupal.org>
2021-04-29 20:49:29 +0000

    Adjust previewer styling

--- js/components/responsive-preview.js
+++ js/components/responsive-preview.js
@@ -6,8 +6,17 @@
 (function ($, _, Drupal, drupalSettings) {
   "use strict";
 
+  // Do all the things to kill our previewer.
+  $(document).on("keyup", function (e) {
+    if (e.key == "Escape") {
+      if ($('.bs-responsive-preview-wrapper').length) {
+        destroyPreviewer();
+      }
+    }
+  });
+
   /**
-   * 
+   *
    */
   function setTempStore(key, value) {
     $.ajax({
@@ -22,7 +31,7 @@
   }
 
   /**
-   * 
+   * Load up our responsive plugin previewer.
    */
   function loadPreviewer(currentDevice) {
     var devices = {
@@ -41,38 +50,40 @@
     }
 
     // Close any old dialogs.
-    Drupal.dialog($('#bs-responsive-preview-wrapper').get(0)).close();
+    destroyPreviewer();
 
     if (currentDevice == 'all') {
       return;
     }
 
     var $clonedDom = $('html').clone();
-    
-    var $iframeWrapper = $('<div id="bs-responsive-preview-wrapper" class="bs-responsive-preview-wrapper"></div>');
+
+    var $iframeWrapper = $('' +
+      '<div id="bs-responsive-preview-wrapper" class="bs-responsive-preview-wrapper">' +
+      '<div class="bs-responsive-preview-scroll-track">' +
+      '<div class="bs-responsive-preview-scroll-pane">' +
+      '<div class="bs-responsive-preview-container">' +
+      '</div>' +
+      '</div>' +
+      '</div>' +
+      '</div>');
 
     var $iframe = $('<iframe id="bs-responsive-preview" width="100%" height="100%" frameborder="0" scrolling="auto" allowtransparency="true"></iframe>')
-      .on('load', function() {
+      .on('load', function () {
         var highlightedSection = $(this).contents().find("#bs-preview-highlighted");
-        var titleBarHight = $('.bs-responsive-preview-dialog .ui-dialog-titlebar').outerHeight();
-        this.contentWindow.scrollTo(0, highlightedSection.offset().top - titleBarHight);
+        this.contentWindow.scrollTo(0, highlightedSection.offset().top);
       });
 
-    $iframeWrapper.append($iframe);
+    // append the iframe to our deepest div
+    $iframeWrapper.find('.bs-responsive-preview-container').append($iframe);
 
-    var $previewDialog = $($iframeWrapper).appendTo('body');
-    Drupal.dialog($previewDialog, {
-      dialogClass: 'bs-responsive-preview-dialog ui-dialog-off-canvas ui-dialog-position-side',
-      title: currentDevice + ' preview',
-      width: devices[currentDevice],
-      height: '600'
-    },
-    ).showModal();
-    
-    $('.bs-responsive-preview-dialog').css('left', '60px !important');
-    
-    // Make bs overlay unique.
-    $('.ui-widget-overlay').addClass('bs-ui-widget-overlay');
+    // append the whole previewer to the window
+    $($iframeWrapper).appendTo('body');
+
+    // set the device width
+    $iframeWrapper.find('.bs-responsive-preview-container').css({
+      'width': devices[currentDevice],
+    });
 
     // Add Id selector to the highlighted section.
     $clonedDom.find('.is-layout-builder-highlighted').attr('id', 'bs-preview-highlighted');
@@ -104,37 +115,75 @@
 
     // Remove style attr with its padding.
     $clonedDom.find('.dialog-off-canvas-main-canvas').removeAttr('style');
-    
+
     // Write the iframe content.
     var $filterdHtml = $clonedDom.html();
     var $iframeDoc = $iframe[0].contentDocument || $iframe[0].contentWindow.document;
     $iframeDoc.write($filterdHtml);
-    $iframe.contents().find( '.bs-ui-widget-overlay').remove();
+    $iframe.contents().find('.bs-ui-widget-overlay').remove();
     $iframeDoc.close();
+
+    setPreviewerSize();
   }
 
+  /**
+   * Close our responsive plugin previewer.
+   */
+  function destroyPreviewer() {
+    $('#bs-responsive-preview-wrapper').remove();
+  }
+
+  /**
+   * Sets the viewport width and height dimensions on the envModel.
+   */
+  function setPreviewerSize() {
+    if($('#drupal-off-canvas').length > 0) {
+      var viewportWidth = document.documentElement.clientWidth;
+      var offcanvasWidth = $('#drupal-off-canvas').css('width');
+      var toolbarHeight = $('.ui-dialog-off-canvas').css('top');
+
+      $('.bs-responsive-preview-container').css('max-width', (viewportWidth - offcanvasWidth));
+      $('.bs-responsive-preview-scroll-pane').css({
+        'padding-right': offcanvasWidth,
+        'padding-top': toolbarHeight
+      });
+    }
+  }
+
+  // Init all of our responsive previewer stuff.
   Drupal.behaviors.bootstrapStylesResponsivePreview = {
     attach: function (context) {
+
       // Listen to bs_responsive device click.
       $("input.bs_responsive", context).once().on("click", function () {
-        console.log('loaded');
         var currentDevice = $(this).val();
         // set it in the temp store.
         setTempStore('active_device', currentDevice);
         loadPreviewer(currentDevice);
       });
+
+      // Close our previewer if the offcanvas menu is closed.
+      $(document, context).once().on('click', '.ui-dialog-titlebar-close', function () {
+        destroyPreviewer();
+      });
+
+      // Our resize handler.
+      $(window).on('load resize', Drupal.debounce(function () {
+        setPreviewerSize();
+      }, 300));
     }
   };
+
 
   /**
    * Refresh responsive command.
    */
-  Drupal.AjaxCommands.prototype.bs_refresh_responsive = function (ajax, response, status) { 
+  Drupal.AjaxCommands.prototype.bs_refresh_responsive = function (ajax, response, status) {
     var currentDevice = response.data.active_device;
     if (currentDevice != 'all') {
-      $('input.bs_responsive[value='+ currentDevice +']').click();
+      $('input.bs_responsive[value=' + currentDevice + ']').click();
     }
-  }
+  };
 
   $(window).on({
     'dialog:beforecreate': function dialogBeforecreate(event, dialog, $element) {
