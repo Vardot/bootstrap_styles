e50f9a0e71b15c06a4d2e944f9425b3a3f130adc
Ahmad Abbad <ah.abbad@vardot.com>
2023-09-24 16:19:15 +0300

    Issue #3389376: Fix border color in layout builder (Blocks,section)

--- js/plugins/border/layout-builder-form.js
+++ js/plugins/border/layout-builder-form.js
@@ -20,26 +20,30 @@
 
       // Assign border style.
       var directions = ['left', 'top', 'right', 'bottom'];
+
       // Loop through the directions.
       for (var i = 0; i < directions.length; i++) {
-        once('blb_border','input.bs-field-border-style-' + directions[i], context).forEach(function (value,i) {
+        var direction = directions[i];
+
+        // Update border style.
+        once('blb_border', 'input.bs-field-border-style-' + direction, context).forEach(function (value) {
           var border_style = '';
-          if ($(value).val() !='_none' && typeof $(value).next('label').css('border-style') != 'undefined') {
-            border_style = $(value).next('label').css('border-' + directions[i] + '-style');
+          if ($(value).val() !== '_none' && typeof $(value).next('label').css('border-style') !== 'undefined') {
+            border_style = $(value).next('label').css('border-' + direction + '-style');
             $(value).next('label').css('border-style', border_style);
           }
         });
 
         // Switch border color to background color.
-        once('blb_border','input.bs-field-border-color-' + directions[i], context).forEach(function (value,i) {
+        once('blb_border', 'input.bs-field-border-color-' + direction, context).forEach(function (value) {
           var border_color = '';
-          if ($(value).val() !='_none' && typeof $(value).next('label').css('border-color') != 'undefined') {
-            border_color = $(value).next('label').css('border-' + directions[i] + '-color');
+          if ($(value).val() !== '_none' && typeof $(value).next('label').css('border-color') !== 'undefined') {
+            border_color = $(value).next('label').css('border-' + direction + '-color');
             $(value).next('label').attr('style', 'background-color: ' + border_color + ' !important; border-color: white !important;');
           }
         });
-
       }
+
     }
   };
 
